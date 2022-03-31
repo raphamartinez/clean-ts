937182ef7f35dbb08a2004c790d2be503e32c183
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-03-31 10:21:21 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: 'babel',
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-};
+  }
+}
