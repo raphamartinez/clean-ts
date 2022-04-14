ad7507ce0482259a9883f08f6b537ce72e9ee53e
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-04-14 11:41:00 -0300

    refactor: change collection of mongodb to async

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,16 +2,19 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
 
   async disconnect (): Promise<void> {
     this.client.close()
+    this.client = null
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
     return this.client.db().collection(name)
   },
 
